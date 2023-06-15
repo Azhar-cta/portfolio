804e3ba4ad1876a4db0b4b91933851ee89ab9400
azhar-cta <azharcta@gmail.com>
2023-06-15 19:47:31 +0530

    commit

---
+++ android/app/src/main/kotlin/com/fluttermapp/portfolio/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.fluttermapp.portfolio
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/home.dart
@@ -0,0 +1,191 @@
+import 'package:flutter/material.dart';
+import 'package:sliding_sheet/sliding_sheet.dart';
+
+class MyHome extends StatefulWidget {
+  const MyHome({Key? key}) : super(key: key);
+
+  @override
+  State<MyHome> createState() => _MyHomeState();
+}
+
+class _MyHomeState extends State<MyHome> {
+  myAchiev(num, type) {
+    return Row(
+      children: [
+        Text(
+          num,
+          style: const TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
+        ),
+        Container(
+          margin: const EdgeInsets.only(top: 10),
+          child: Text(type),
+        ),
+      ],
+    );
+  }
+
+  mySpec(
+    icon,
+    tech,
+  ) {
+    return Container(
+      width: 105,
+      height: 115,
+      child: Card(
+        margin: const EdgeInsets.all(0),
+        color: const Color(0xff252525),
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(15),
+        ),
+        child: Container(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Icon(
+                icon,
+                color: Colors.white,
+              ),
+              const SizedBox(
+                height: 10,
+              ),
+              Text(
+                tech,
+                style: const TextStyle(fontSize: 16, color: Colors.white),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.black,
+      extendBodyBehindAppBar: true,
+      appBar: AppBar(
+        elevation: 0,
+        backgroundColor: Colors.transparent,
+      ),
+      body: SlidingSheet(
+        elevation: 8,
+        cornerRadius: 40,
+        snapSpec: const SnapSpec(
+          // Enable snapping. This is true by default.
+          snap: true,
+          // Set custom snapping points.
+          snappings: [0.38, 0.7, 1.0],
+          // Define to what the snappings relate to. In this case,
+          // the total available space that the sheet can expand to.
+          positioning: SnapPositioning.relativeToAvailableSpace,
+        ),
+        // The body widget will be displayed under the SlidingSheet
+        // and a parallax effect can be applied to it.
+        body: Container(
+          child: Stack(
+            children: [
+              //image
+              Container(
+                child: Image.asset('assets/azhar.png'),
+
+              ),
+              Container(
+                alignment: Alignment.center,
+                margin: EdgeInsets.only(
+                    top: MediaQuery.of(context).size.height * 0.52),
+                child: const Column(
+                  children: [
+                    Text(
+                      'Azhar',
+                      style: TextStyle(
+                          color: Colors.white,
+                          fontWeight: FontWeight.bold,
+                          fontSize: 40),
+                    ),
+                    Text(
+                      'Software Developer',
+                      style: TextStyle(
+                          color: Colors.white,
+                          fontWeight: FontWeight.bold,
+                          fontSize: 25),
+                    ),
+                  ],
+                ),
+              )
+            ],
+          ),
+        ),
+        builder: (context, state) {
+          // This is the content of the sheet that will get
+          // scrolled, if the content is bigger than the available
+          // height of the sheet.
+          return Container(
+            margin: const EdgeInsets.only(
+              left: 20,
+              right: 20,
+              top: 30,
+            ),
+            height: 500,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    myAchiev('10', 'Projects'),
+                    myAchiev('64', 'Clients'),
+                    myAchiev('92', 'Messages'),
+                  ],
+                ),
+                const SizedBox(
+                  height: 30.0,
+                ),
+                const Text(
+                  'Specialized In',
+                  style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+                ),
+                const SizedBox(
+                  height: 10,
+                ),
+                Column(
+                  children: [
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        mySpec(Icons.android, 'Andoid'),
+                        mySpec(Icons.window_sharp, 'Windows'),
+                        mySpec(Icons.apple, 'Apple'),
+                      ],
+                    ),
+                    const SizedBox(
+                      height: 10,
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        mySpec(Icons.android, 'Andoid'),
+                        mySpec(Icons.android, 'Andoid'),
+                        mySpec(Icons.android, 'Andoid'),
+                      ],
+                    ),
+                    const SizedBox(
+                      height: 10,
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        mySpec(Icons.android, 'Andoid'),
+                        mySpec(Icons.android, 'Andoid'),
+                        mySpec(Icons.android, 'Andoid'),
+                      ],
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          );
+        },
+      ),
+    );
+  }
+}
